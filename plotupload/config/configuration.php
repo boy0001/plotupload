cc2ddc25bc042e883b613bd37cdfb30357b0192b
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-04 01:43:18 -0500

    gtg to bed lmao

--- plotupload/config/configuration.php
+++ plotupload/config/configuration.php
@@ -22,7 +22,9 @@ $config = array();                                                              
                                                                                                                        #
 # Edit below this ######################################################################################################
 $config['title'] = 'PlotSquared plot download'                            ; # Title of the web page                    #
-$config['style'] = $styles[array_rand($styles, 1)]                        ; # The style to use (see styles folder)     #
+//For A Specific Style:																								   #
+$config['style'] = 'default'											  ; # The style to use (see styles folder)	   #
+//For Random Styles: $config['style'] = $styles[array_rand($styles, 1)]   										       #
 //If you like a specific style: $config['style'] = 'default'                                                           #
                                                                                                                        #
 $config['home']  = 'https://www.spigotmc.org/resources/plotsquared.1177/' ; # The website for the home link            #
--- plotupload/index.php
+++ plotupload/index.php
@@ -9,22 +9,87 @@ if (isset($arr["key"])) {
 }
 ?>
 <HTML>
-<HEAD>
-<TITLE><?php echo Config::get('title');?></TITLE>
-<link rel='stylesheet' type='text/css' href='style/style_<?php echo Config::get('style');?>.css'>
-</HEAD>
+	<HEAD>
+		<TITLE><?php echo Config::get('title');?></TITLE>
+		<link rel='stylesheet' type='text/css' href='style/style_<?php echo Config::get('style');?>.css'>
+		<!-- BEGIN Custom Themes -->
+			<!-- Yeti - DEFAULT -->
+				<!-- For Development -->
+				<link rel="stylesheet" href="res/themes/Yeti/css/bootstrap.min.css?version=2" title="Yeti">
+				<link rel="stylesheet" href="res/themes/Yeti/css/custom.css?version=2" title="Yeti">
+				<link rel="stylesheet" href="res/themes/Yeti/css/font-awesome.min.css?version=2" title="Yeti">
+				<!-- Live CSS -->
+				<link rel="stylesheet" href="/styles/themes/Yeti/css/bootstrap.min.css?version=2" title="Yeti">
+				<link rel="stylesheet" href="/styles/themes/Yeti/css/custom.css?version=2" title="Yeti">
+				<link rel="stylesheet" href="/styles/themes/Yeti/css/font-awesome.min.css?version=2" title="Yeti">
+		<!-- END Custom Themes -->
+	</HEAD>
 <BODY>
 
 <!--
 EDITABLE: Navigation
 -->
-<div id=nav>
-<div id=button><a class=navlink href="<?php echo Config::get('home');?>">Home</a></div>
-<div id=button><a class=navlink href="https://github.com/IntellectualCrafters/PlotSquared/wiki">Wiki</a></div>
-<div id=button><a class=navlink href="https://github.com/IntellectualCrafters/PlotSquared/issues">Issues</a></div>
-</div>
-<div id=banner>
-</div>
+<!-- Banner Image -->
+<div id=banner></div>
+<!-- CUSTOM NAVBAR -->
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main_navbar_collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="./">CP2 Interface</a>
+		</div>
+		
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="main_navbar_collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="<?php echo Config::get('home');?>">Home</a></li>
+				<li><a href="https://github.com/IntellectualCrafters/PlotSquared/wiki">Wiki</a></li>
+				<li><a href="https://github.com/IntellectualCrafters/PlotSquared/issues">Issues</a><li>
+			</ul>
+			<!-- User control (right side of navbar) -->
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown alert-dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="margin: -10px 0px; font-size: 16px;"><i class="fa fa-envelope"></i> <div style="display: inline;" id="pms"></div></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Click View Messages</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="../user/messaging">View Messages</a></li>
+					</ul>
+				</li>	
+				
+				<li class="dropdown alert-dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="margin: -10px 0px; font-size: 16px;"><i class="fa fa-flag"></i> <div style="display: inline;" id="alerts"></div></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Click View Alerts</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="../user/alerts">View Alerts</a></li>
+					</ul>
+				</li>
+				
+				<li class="dropdown">
+					<a href="#" data-toggle="dropdown" role="button" aria-expanded="false">&nbsp;&nbsp;MuhsinunCool <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="../profile/<?php echo $this->u;?>">Profile</a></li>
+						<li class="divider"></li>
+						<li><a href="../user">UserCP</a></li>
+						<li><a href="../mod">ModCP</a></li>
+						<li><a href="../admin">AdminCP</a></li>
+						<li class="divider"></li>
+						<li><a href="../infractions">Infractions</a></li>
+						<li class="divider"></li>
+						<li><a href="./login.php<?php if ($this->u) echo "?action=logout"?>"><?php echo $this->u ? "Sign Out" : "Sign In";?></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container -->
+</nav>
 <!--
 END NAVIGATION
 -->
@@ -36,6 +101,19 @@ else {
     echo "<h1>PlotSquared plot downloading</h1>";
 }
 ?>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Consoles</h3>
+	</div>
+	<div class="panel-body">
+		<ul>
+			<li><a href="#">Hub</a></li>
+			<li><a href="#">Creative</a></li>
+		</ul>
+	</div>
+</div>
+
 <div id="main">
 <div id=box>
 <h2>How to download your plot</h2>
